9cbe4955781e2678cc2ca6b28ac5d4144e184b72
Vladislav Sosiuk <v.sosiuk@alterplay.com>
2020-10-30 11:15:58 +0200

    Add post_install implementation for supporting Preview

---
+++ SupportingFiles/post_install_for_preview_support.rb
@@ -0,0 +1,29 @@
+post_install do |installer|
+    project_file_name = Dir.glob("*.xcodeproj").first
+    project = Xcodeproj::Project.open(project_file_name)
+    project_targets = project.native_targets
+    pod_target_name_prefix = "Pods-"
+    project_targets.each do |project_target| 
+        aggregate_project_pod_target = installer.aggregate_targets.find do |pod_target|
+            pod_target.name == pod_target_name_prefix + project_target.name 
+        end
+        project_target.build_configurations.each do |project_target_build_configuration|
+            aggregate_project_pod_target.xcconfigs.find do |xcconfig_name, xcconfig_file|
+                if xcconfig_name == project_target_build_configuration.name
+                    framework_search_paths = xcconfig_file.attributes['FRAMEWORK_SEARCH_PATHS']
+                    default_search_paths = '@executable_path/Frameworks @loader_path/Frameworks '
+                    project_target_build_configuration.build_settings['LD_RUNPATH_SEARCH_PATHS[sdk=iphonesimulator*]'] = default_search_paths + framework_search_paths
+                    project_target_build_configuration.build_settings['SWIFT_OPTIMIZATION_LEVEL[sdk=iphonesimulator*]'] = "-Onone";
+                end
+            end
+        end
+        project_pod_target = installer.pods_project.targets.find do |pod_target|
+            pod_target.name == pod_target_name_prefix + project_target.name
+        end
+        project_pod_target.build_configurations.each do |config|
+            config.build_settings['MACH_O_TYPE[sdk=iphonesimulator*]'] = "mh_dylib"
+        end
+    end
+    project.save
+  end
+  
